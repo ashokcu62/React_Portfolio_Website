2effb053c2e4ba0a78540277e8c8fbc3016abbce
ashokcu62 <ashokcu62@gmail.com>
2022-09-05 17:54:07 +0530

    footer completed

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import './Footer.css'
+import { FaFacebookSquare, FaInstagramSquare, FaTwitterSquare } from 'react-icons/fa'
 const Footer = () => {
   return (
-    <div>
-      footer
-    </div>
+    <section>
+      <footer>
+        <a href="" className="footer__logo">ASHOK</a>
+
+
+        <ul className="permalinks">
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">about</a></li>
+          <li><a href="#experience<">Experience</a></li>
+          <li><a href="#service">Service</a></li>
+          <li><a href="#portfolio">Portfolio</a></li>
+          <li><a href="#testimonial">Testimonial</a></li>
+        </ul>
+
+        <div className="footer__socials">
+          <a href="https://facebook.com"><FaFacebookSquare /></a>
+          <a href="https://instagram.com"><FaInstagramSquare /></a>
+          <a href="https://twiter.com"><FaTwitterSquare /></a>
+        </div>
+        <div className="footer__copyright">
+          <small>&copy; ashokcu.All rights reserved.</small>
+        </div>
+      </footer>
+    </section>
   )
 }
 
